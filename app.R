005ce9ab7f6a9c220666d4750ef9a393b14a2bb6
Joli Holmes <jh111@ad.rice.edu>
2020-03-19 16:09:48 -0500

    added here::here to resolve project root bug

--- app.R
+++ app.R
@@ -1,9 +1,31 @@
+library(magrittr)
+library(ggplot2)
+
 rm(list = ls())
 options(encoding = "UTF-8")
 
 sapply(list.files("R", full.names = TRUE, recursive = TRUE), source, .GlobalEnv)
 
 config <- yaml::read_yaml("./mainDashboard.yaml")
+
+confirmed <- read.csv(here::here("./data/usafacts/covid_confirmed.csv"),
+                      fileEncoding="UTF-8-BOM",
+                      stringsAsFactors = FALSE) %>%
+  tidyr::gather(Date, confirmed, -c(countyFIPS, County.Name, State, stateFIPS))
+
+deaths <- read.csv(here::here("./data/usafacts/covid_deaths.csv"),
+                   fileEncoding="UTF-8-BOM",
+                   stringsAsFactors = FALSE) %>%
+  tidyr::gather(Date, deaths, -c(countyFIPS, County.Name, State, stateFIPS))
+
+county <- create_data.county(confirmed_df = confirmed,
+                             deaths_df = deaths)
+
+state <- create_data.state(confirmed_df = confirmed,
+                           deaths_df = deaths)
+
+usa <- create_data.usa(confirmed_df = confirmed,
+                       deaths_df = deaths)
 
 ui <- tpl_ui(title = config$ui$dashboardtitle$title
             ,tabs = config$ui$tabs
