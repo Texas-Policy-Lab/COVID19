d75bda9889f3771aa2973337d1e2daa4f41cc8d4
Joli Holmes <jh111@ad.rice.edu>
2020-03-20 13:50:52 -0500

    added whitespace

--- R/data_management.R
+++ R/data_management.R
@@ -7,16 +7,16 @@ create_data.default <- function(confirmed_df, deaths_df, ...) {
     dplyr::mutate(Date = gsub("X", "", Date)
                   ,Date = gsub("[.]", "/", Date)
                   ,Date = lubridate::mdy(Date))
-  
+
 }
 create_data.county <- function(confirmed_df, deaths_df) {
-  
+
   county <- create_data.default(confirmed_df, deaths_df) %>%
     dplyr::arrange(Date, State) %>%
     dplyr::left_join(census.county_pop()) %>%
     dplyr::mutate(confirm_per_100k = (confirmed/pop)*100000
                   ,death_per100k = (deaths/pop)*100000)
-  
+
   return(county)
 }
 
@@ -31,8 +31,8 @@ create_data.state <- function(confirmed_df, deaths_df, geocodes) {
     dplyr::left_join(census.state_pop()) %>%
     dplyr::left_join(testing()) %>%
     dplyr::mutate(confirm_per_100k = (confirmed/pop)*100000
-                  ,deaths_per100k = (deaths/pop)*100000
-                  ,tests_per_100k = (total_tests/pop)*100000
+                 ,deaths_per100k = (deaths/pop)*100000
+                 ,tests_per_100k = (total_tests/pop)*100000
     )
 
   state[is.na(state)] <- 0
@@ -53,7 +53,7 @@ create_data.usa <- function(confirmed_df, deaths_df) {
 fips_xwalk <- function(geocodes) UseMethod("fips_xwalk")
 
 fips_xwalk.state <- function(geocodes) {
-  
+
   geocodes %>%
     dplyr::filter(SummaryLevel == 40) %>% 
     dplyr::select(stateFIPS, label) %>% 
