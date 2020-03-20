1b2fc58bff3d193fcc3aea0f250f9d77b1ae9b01
Connor Rothschild <connorrothschild@gmail.com>
2020-03-20 13:53:16 -0500

    add a tx county map

--- R/plot_eda.R
+++ R/plot_eda.R
@@ -61,3 +61,40 @@ county %>%
   ggplot(aes(x = Date, y = confirmed, fill = County.Name)) +
   geom_col(position = "stack") +
   scale_x_date(limits = c(plot_start_date, max_date))
+
+tx_counties <- map_data("county") %>% filter(region == "texas") %>% mutate(subregion = str_to_title(subregion))
+tx_county_stats <- county %>% 
+  filter(State == "TX") %>% 
+  group_by(County.Name) %>% 
+  summarise(confirmed = sum(confirmed), deaths = sum(deaths)) %>% 
+  mutate(County.Name = gsub("County", "", County.Name),
+         County.Name = str_trim(County.Name))
+
+merged <- full_join(tx_county_stats, tx_counties, by = c("County.Name" = "subregion"))
+
+ggplot(data = merged, mapping = aes(x = long, y = lat, group = County.Name, fill = confirmed)) +
+  coord_fixed(1.3) +
+  geom_polygon(color = "black") +
+  labs(fill = "Confirmed Cases") +
+  # ggmap::theme_nothing(legend = TRUE) +
+  theme(legend.title = element_text(),
+        legend.key.height = unit(.1, "in"),
+        panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_blank(), 
+        axis.text.x = element_blank(), axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        text = element_text(family = "Proxima Nova"),
+        legend.position = "bottom") +
+  labs(x = element_blank(),
+       y = element_blank(),
+       title = "Confirmed COVID-19 Cases in Texas") +
+  theme(
+  axis.text = ggplot2::element_blank(),
+  axis.ticks = ggplot2::element_blank(),
+  axis.title = ggplot2::element_blank(),
+  panel.grid = ggplot2::element_blank(),
+  axis.line = ggplot2::element_blank(),
+  legend.margin = ggplot2::margin(t = 6L, r = 6L, b = 6L)
+)
+
