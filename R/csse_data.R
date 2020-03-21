1a9ba572d59b76901035e100c2c612c8edb3e53a
Joli Holmes <jh111@ad.rice.edu>
2020-03-20 19:01:44 -0500

    initial commit

---
+++ R/csse_data.R
@@ -0,0 +1,20 @@
+#' @title CSSE confirmed cases data
+csse_data.confirmed <- function(url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") {
+ 
+  df <- read.csv(url) %>% 
+    tidyr::gather(Date, confirmed, -c(Province.State, Country.Region, Lat, Long)) %>% 
+    dplyr::group_by(Date, Country.Region) %>% 
+    dplyr::summarise(confirmed = sum(confirmed))
+  
+    
+}
+
+#' @title CSSE deaths data
+csse_data.deaths <- function(url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv") {
+  
+  df <- read.csv(url) %>% 
+    tidyr::gather(Date, deaths, -c(Province.State, Country.Region, Lat, Long)) %>% 
+    dplyr::group_by(Date, Country.Region) %>% 
+    dplyr::summarise(deaths = sum(deaths))
+  
+}
