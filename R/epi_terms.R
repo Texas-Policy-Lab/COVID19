9242055d3e4e5c3fce821801878934b7b1ea892e
Joli Holmes <jh111@ad.rice.edu>
2020-03-24 17:00:20 -0500

    initial commit

---
+++ R/epi_terms.R
@@ -0,0 +1,12 @@
+terms.ui <- function() {
+  shiny::tableOutput("epi_table")  
+}
+
+terms.server <- function(input, output, session, epi_pth = "data/epi_terms.csv") {
+  
+  output$epi_table <- shiny::renderTable({
+    read.csv(here::here(epi_pth),
+             stringsAsFactors = FALSE)
+  })
+
+}
