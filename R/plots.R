5449ffbfb8d52df96986fcdb26b1e5fd813429c2
Joli Holmes <jh111@ad.rice.edu>
2020-03-24 12:37:09 -0500

    added state_stats plot

--- R/plots.R
+++ R/plots.R
@@ -1,135 +1,135 @@
-line_chart <- function(...) UseMethod ("line_chart")
-
-#' @param df dataframe. The dataframe use
-#' @param x string. The name of the x variable.
-#' @param y string. The name of the y variable.
-#' @param color string. The name of the color variable.
-#' @param title string. The title for the plot
-#' @param x_lab string. The label for the x-axis.
-#' @param y_lab string. The label for the y-axis.
-#' @param legend_lab string. The label for the legend.
-#' @param annotation_text_size integer. The size for the text.
-#' @param label string. The name of the label.
-line_chart.default <- function(df = NULL,
-                               x = NULL,
-                               y = NULL,
-                               color = NULL,
-                               title = NULL,
-                               x_lab = NULL,
-                               y_lab = NULL,
-                               legend_lab = NULL,
-                               annotation_text_size = NULL,
-                               label = NULL,
-                               usafacts_source = "Confirmed COVID-19 cases and deaths: USAFacts Data (https://usafacts.org/)",
-                               census_source = "Population data: 2018 American Community Survey 5-year Estimates",
-                               covid_tracking_source = "Testing data: The Covid Tracking Project (https://covidtracking.com/)",
-                          ...) {
-# browser()
-  x_vec <- df[[x]]
-  y_vec <- df[[y]]
-
-  if(!is.null(color)) color_vec <- df[[color]]
-  if(!is.null(label)) label_vec <- df[[label]]
-
-  if(is.null(color)) {
-
-    if(is.null(label)) {
-      gg <- ggplot(df, aes(x = x_vec, y = y_vec))
-        geom_point(color = "#e54e4d")
-
-    } else{
-      
-      gg <- ggplot(df, aes(x = x, y = y, label = str_wrap(label_vec, width = 65))) +
-        geom_point(color = "#e54e4d") +
-        geom_text_repel(size = 3,
-                        hjust = 0,
-                        nudge_x = 2)
-    }
-
-  } else {
-
-    if(is.null(label)) {
-      
-      gg <- ggplot(df,
-                   aes(x = x_vec, y = y_vec, color = color_vec)) +
-        scale_color_manual(values = as.vector(tpltheme::palette_tpl_main))
-
-    } else {
-      
-      gg <- ggplot(df,
-                   aes(x = x_vec, y = y_vec, color = color_vec,
-                       label = str_wrap(label_vec, width = 65))) +
-        scale_color_manual(values = as.vector(tpltheme::palette_tpl_main)) +
-        geom_text_repel(size = 3,
-                        hjust = 0,
-                        nudge_x = 2)
-      
-
-    }
-  }
-
-  pandemic <- df %>%
-    dplyr::filter(Date == as.Date("2020-01-30"))
-
-  gg <- gg +
-    geom_point() +
-    geom_line() +
-    labs(title = title,
-         x = x_lab,
-         y = y_lab,
-         color = legend_lab,
-         caption = paste(usafacts_source, census_source, covid_tracking_source, sep = "/n")) +
-    geom_vline(xintercept = pandemic$Date, linetype="dashed", 
-               color = "grey", size = 1) +
-    annotate(geom="text", x = pandemic$Date, y = max(df$confirmed) - 1000,
-             label = "WHO declares pandemic", size = annotation_text_size)
-  
-}
-
-line_chart.state.confirmed_cases <- function(state,
-                                             x = "Date",
-                                             y = "confirmed",
-                                             color = "stateName",
-                                             title = "Total # confirmed cases in each state",
-                                             x_lab = "Date",
-                                             y_lab = "Total # cases",
-                                             legend_lab = "State") {
-
-  gg <- line_chart.default(df = state, x = x, y = y, color = color, title = title, x_lab = x_lab, y_lab = y_lab, legend_lab = legend_lab)
-  print(gg)
-}
-
-line_chart.world.confirmed_cases <- function(world,
-                                             x = "Date",
-                                             y = "confirmed",
-                                             color = "county",
-                                             title = "Total # confirmed cases in each state",
-                                             x_lab = "Date",
-                                             y_lab = "Total # cases",
-                                             legend_lab = "State") {
-  
-  gg <- line_chart.default(df = world, x = x, y = y, color = color, title = title, x_lab = x_lab, y_lab = y_lab, legend_lab = legend_lab)
-  print(gg)
-}
-
-
-line_chart.china <- function(china,
-                             x = "Date",
-                             y = "confirmed",
-                             color = NULL,
-                             title = "Total # confirmed cases in China",
-                             x_lab = "Date",
-                             y_lab = "Total # cases",
-                             label = "label",
-                             legend_lab = NULL,
-                             annotation_text_size = 3) {
-  
-  gg <- line_chart.default(df = china, x = x, y = y, color = color, title = title,
-                           x_lab = x_lab, y_lab = y_lab, legend_lab = legend_lab,
-                           annotation_text_size = annotation_text_size,
-                           label = label)
-  return(gg)
-}
+#' line_chart <- function(...) UseMethod ("line_chart")
+#' 
+#' #' @param df dataframe. The dataframe use
+#' #' @param x string. The name of the x variable.
+#' #' @param y string. The name of the y variable.
+#' #' @param color string. The name of the color variable.
+#' #' @param title string. The title for the plot
+#' #' @param x_lab string. The label for the x-axis.
+#' #' @param y_lab string. The label for the y-axis.
+#' #' @param legend_lab string. The label for the legend.
+#' #' @param annotation_text_size integer. The size for the text.
+#' #' @param label string. The name of the label.
+#' line_chart.default <- function(df = NULL,
+#'                                x = NULL,
+#'                                y = NULL,
+#'                                color = NULL,
+#'                                title = NULL,
+#'                                x_lab = NULL,
+#'                                y_lab = NULL,
+#'                                legend_lab = NULL,
+#'                                annotation_text_size = NULL,
+#'                                label = NULL,
+#'                                usafacts_source = "Confirmed COVID-19 cases and deaths: USAFacts Data (https://usafacts.org/)",
+#'                                census_source = "Population data: 2018 American Community Survey 5-year Estimates",
+#'                                covid_tracking_source = "Testing data: The Covid Tracking Project (https://covidtracking.com/)",
+#'                           ...) {
+#' # browser()
+#'   x_vec <- df[[x]]
+#'   y_vec <- df[[y]]
+#' 
+#'   if(!is.null(color)) color_vec <- df[[color]]
+#'   if(!is.null(label)) label_vec <- df[[label]]
+#' 
+#'   if(is.null(color)) {
+#' 
+#'     if(is.null(label)) {
+#'       gg <- ggplot(df, aes(x = x_vec, y = y_vec))
+#'         geom_point(color = "#e54e4d")
+#' 
+#'     } else{
+#'       
+#'       gg <- ggplot(df, aes(x = x, y = y, label = str_wrap(label_vec, width = 65))) +
+#'         geom_point(color = "#e54e4d") +
+#'         geom_text_repel(size = 3,
+#'                         hjust = 0,
+#'                         nudge_x = 2)
+#'     }
+#' 
+#'   } else {
+#' 
+#'     if(is.null(label)) {
+#'       
+#'       gg <- ggplot(df,
+#'                    aes(x = x_vec, y = y_vec, color = color_vec)) +
+#'         scale_color_manual(values = as.vector(tpltheme::palette_tpl_main))
+#' 
+#'     } else {
+#'       
+#'       gg <- ggplot(df,
+#'                    aes(x = x_vec, y = y_vec, color = color_vec,
+#'                        label = str_wrap(label_vec, width = 65))) +
+#'         scale_color_manual(values = as.vector(tpltheme::palette_tpl_main)) +
+#'         geom_text_repel(size = 3,
+#'                         hjust = 0,
+#'                         nudge_x = 2)
+#'       
+#' 
+#'     }
+#'   }
+#' 
+#'   pandemic <- df %>%
+#'     dplyr::filter(Date == as.Date("2020-01-30"))
+#' 
+#'   gg <- gg +
+#'     geom_point() +
+#'     geom_line() +
+#'     labs(title = title,
+#'          x = x_lab,
+#'          y = y_lab,
+#'          color = legend_lab,
+#'          caption = paste(usafacts_source, census_source, covid_tracking_source, sep = "/n")) +
+#'     geom_vline(xintercept = pandemic$Date, linetype="dashed", 
+#'                color = "grey", size = 1) +
+#'     annotate(geom="text", x = pandemic$Date, y = max(df$confirmed) - 1000,
+#'              label = "WHO declares pandemic", size = annotation_text_size)
+#'   
+#' }
+#' 
+#' line_chart.state.confirmed_cases <- function(state,
+#'                                              x = "Date",
+#'                                              y = "confirmed",
+#'                                              color = "stateName",
+#'                                              title = "Total # confirmed cases in each state",
+#'                                              x_lab = "Date",
+#'                                              y_lab = "Total # cases",
+#'                                              legend_lab = "State") {
+#' 
+#'   gg <- line_chart.default(df = state, x = x, y = y, color = color, title = title, x_lab = x_lab, y_lab = y_lab, legend_lab = legend_lab)
+#'   print(gg)
+#' }
+#' 
+#' line_chart.world.confirmed_cases <- function(world,
+#'                                              x = "Date",
+#'                                              y = "confirmed",
+#'                                              color = "county",
+#'                                              title = "Total # confirmed cases in each state",
+#'                                              x_lab = "Date",
+#'                                              y_lab = "Total # cases",
+#'                                              legend_lab = "State") {
+#'   
+#'   gg <- line_chart.default(df = world, x = x, y = y, color = color, title = title, x_lab = x_lab, y_lab = y_lab, legend_lab = legend_lab)
+#'   print(gg)
+#' }
+#' 
+#' 
+#' line_chart.china <- function(china,
+#'                              x = "Date",
+#'                              y = "confirmed",
+#'                              color = NULL,
+#'                              title = "Total # confirmed cases in China",
+#'                              x_lab = "Date",
+#'                              y_lab = "Total # cases",
+#'                              label = "label",
+#'                              legend_lab = NULL,
+#'                              annotation_text_size = 3) {
+#'   
+#'   gg <- line_chart.default(df = china, x = x, y = y, color = color, title = title,
+#'                            x_lab = x_lab, y_lab = y_lab, legend_lab = legend_lab,
+#'                            annotation_text_size = annotation_text_size,
+#'                            label = label)
+#'   return(gg)
+#' }
 
 pandemic_declared <- function(gg, df, 
                               text1 = "WHO announces that the new coronavirus disease will be called 'COVID-19'",
@@ -238,4 +238,60 @@ timeline.default <- function(df,
   gg <- pandemic_declared(gg = gg, df = df)
 
   print(gg)
+}
+
+
+state_stats <- function(...) UseMethod("state_stats")
+
+state_stats.default <- function(df,
+                                x = "Date",
+                                y = NULL,
+                                color = NULL,
+                                y_lab = NULL,
+                                x_lab = "Date",
+                                legend_lab = "States",
+                                usafacts_source = "Confirmed COVID-19 cases and deaths: USAFacts Data (https://usafacts.org/)") {
+  
+  x <- df[[x]]
+  y <- df[[y]]
+  color <- df[[color]]
+  
+  ggplot(df, aes(x = x, y = y, color = color)) +
+    geom_line() +
+    geom_point() +
+    scale_color_manual(legend_lab,
+                       values = as.vector(tpltheme::tpl_palettes$categorical)) + 
+    labs(x = x_lab,
+         y = y_lab,
+         caption = paste("Source:", usafacts_source))
+} 
+
+state_stats.confirmed <- function(df) {
+  
+  cls <- list(y_lab = "# confirmed cases",
+              y = "confirmed",
+              color = "stateName",
+              df = df)
+
+  do.call(state_stats, cls)
+}
+
+state_stats.deaths <- function(df) {
+  
+  cls <- list(y_lab = "# deaths",
+              y = "deaths",
+              color = "stateName",
+              df = df)
+  
+  do.call(state_stats, cls)
+}
+
+state_stats.tests <- function(df) {
+  
+  cls <- list(y_lab = "# tests",
+              y = "total_tests",
+              color = "stateName",
+              df = df)
+  
+  do.call(state_stats, cls)
 }
