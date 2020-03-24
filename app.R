9ee4db0c103fd32894236b79c386533e3fa390b4
Joli Holmes <jh111@ad.rice.edu>
2020-03-24 09:57:31 -0500

    moved classes into app.R because they rely on state and world level data

--- app.R
+++ app.R
@@ -16,6 +16,61 @@ usa <- create_data.usa()
 
 world <- create_data.world()
 
+timeline_tx_cls <- structure(list(df = state,
+                                  state = "Texas",
+                                  title = "Confirmed cases in the state of {} over time",
+                                  hjust = 1,
+                                  nudge_x = 20,
+                                  box_padding = 1),
+                             class = "tx")
+
+timeline_ca_cls <- structure(list(df = state,
+                                  state = "California",
+                                  title = "Confirmed cases in the state of {} over time",
+                                  str_width = 100,
+                                  hjust = 1,
+                                  nudge_x = 20,
+                                  box_padding = 1),
+                             class = "ca")
+
+timeline_ny_cls <- structure(list(df = state,
+                                  state = "New York",
+                                  title = "Confirmed cases in the state of {} over time",
+                                  str_width = 100,
+                                  hjust = 1,
+                                  nudge_x = 20,
+                                  box_padding = 1),
+                             class = "ny")
+
+timeline_us_cls <- structure(list(df = world %>% 
+                                    dplyr::filter(is.na(stateName)),
+                                  country = "US",
+                                  title = "Confirmed cases in the United States over time",
+                                  hjust = 1,
+                                  nudge_x = 20),
+                             class = "us")
+
+timeline_china_cls <- structure(list(df = world,
+                                     country = "China",
+                                     title = "Confirmed cases in {} over time"),
+                                class = "china")
+
+timeline_italy_cls <- structure(list(df = world,
+                                     country = "Italy",
+                                     title = "Confirmed cases in {} over time",
+                                     str_width = 100,
+                                     hjust = 1,
+                                     nudge_x = 20,
+                                     box_padding = 1),
+                                class = "italy")
+
+timeline_cls_list <- list(tx = timeline_tx_cls,
+                          ca = timeline_ca_cls,
+                          ny = timeline_ny_cls,
+                          us = timeline_us_cls,
+                          china = timeline_china_cls,
+                          italy = timeline_italy_cls)
+
 ui <- tpl_ui(title = config$ui$dashboardtitle$title
             ,tabs = config$ui$tabs
             ,js_pth = list.files(config$js, full.names = TRUE)
