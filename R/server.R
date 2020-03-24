16dc7bf552a1759ff7c30dae604fe1612545c00f
Joli Holmes <jh111@ad.rice.edu>
2020-03-24 12:37:40 -0500

    added additional plots

--- R/server.R
+++ R/server.R
@@ -2,4 +2,5 @@
 server <- function(input, output, session) {
   timeline_world.server(input, output, session)
   timeline_state.server(input, output, session)
+  state_stats.server(input, output, session)
 }
