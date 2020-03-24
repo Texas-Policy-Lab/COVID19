2318c03f99661cffef049c8ecd744fb1e2acd0c5
Joli Holmes <jh111@ad.rice.edu>
2020-03-24 17:58:25 -0500

    deleted whitespace

--- R/epi_terms.R
+++ R/epi_terms.R
@@ -3,7 +3,7 @@ terms.ui <- function() {
 }
 
 terms.server <- function(input, output, session, epi_pth = "data/epi_terms.csv") {
-  
+
   output$epi_table <- shiny::renderTable({
     read.csv(here::here(epi_pth),
              stringsAsFactors = FALSE)
