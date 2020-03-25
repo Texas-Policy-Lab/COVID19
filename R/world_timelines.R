d016f622196ae571c0f9921e7db1324b93887062
Joli Holmes <jh111@ad.rice.edu>
2020-03-25 11:32:39 -0500

    copied in state information

--- R/world_timelines.R
+++ R/world_timelines.R
@@ -192,3 +192,29 @@ timeline_world.server <- function(input, output, session) {
   })
   
 }
+
+timeline_state.ui <- function() {
+  shiny::fluidRow(
+    shiny::column(width = 2,
+                  shiny::radioButtons("choose_state",
+                                      label = shiny::h3("State"),
+                                      width = "110px",
+                                      choices = list("Texas" = "tx",
+                                                     "California" = "ca",
+                                                     "New York" = "ny"),
+                                      selected = "tx")),
+    shiny::column(width = 8,
+                  shiny::plotOutput("timeline_state_plot"))
+    
+  )
+}
+
+timeline_state.server <- function(input, output, session) {
+  
+  output$timeline_state_plot <- shiny::renderPlot({
+    timeline_cls <- timeline_cls_list[[input$choose_state]]
+    do.call(timeline, timeline_cls)
+  })
+  
+}
+
