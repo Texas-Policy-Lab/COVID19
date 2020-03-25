5194917b01db5b07d6a45e029eaf8bb7dcd115cd
Joli Holmes <jh111@ad.rice.edu>
2020-03-25 12:52:09 -0500

    updated world stats.R

--- R/world_stats.R
+++ R/world_stats.R
@@ -1,219 +1,117 @@
-timeline <- function(...) UseMethod ("timeline")
-
-timeline.default <- function(df,
-                             country = NULL,
-                             state = NULL,
-                             color = "#e54e4d",
-                             y_lab = "Confirmed cases",
-                             x_lab = "Date",
-                             title = NULL,
-                             size = 3, 
-                             hjust = 0, 
-                             nudge_x = 8,
-                             box_padding = .5,
-                             point.padding = .2,
-                             direction = "y",
-                             alpha = 1,
-                             str_width = 65,
-                             usafacts_source = "Confirmed COVID-19 cases and deaths: USAFacts Data (https://usafacts.org/)",
-                             census_source = "Population data: 2018 American Community Survey 5-year Estimates",
-                             covid_tracking_source = "Testing data: The Covid Tracking Project (https://covidtracking.com/)",
-                             world_data_source ="Johns Hopkins Center for Systems Science and Engineering  (https://github.com/CSSEGISandData/COVID-19)") {
+country_stats.confirmed <- function(df, alpha) {
   
-  if(!is.null(country)) {
-    df <- df %>% 
-      dplyr::filter(countryName == country)
-    
-    title <- glue::glue(title)
-  }
-  
-  if(!is.null(state)) {
-    df <- df %>%
-      dplyr::filter(stateName == state)
-    
-    title <- glue::glue(title)
-  }
-  
-  gg <- ggplot2::ggplot(df,
-                        ggplot2::aes(x = Date,
-                                     y = confirmed,
-                                     label = stringr::str_wrap(label,
-                                                               width = str_width))) + 
-    ggplot2::geom_point(color = color) +
-    ggplot2::geom_line() +
-    ggplot2::labs(y = y_lab,
-                  x = x_lab,
-                  caption = paste("Source:", world_data_source),
-                  title = title) +
-    ggplot2::scale_y_continuous(labels = scales::comma_format())
-  
-  gg <- text_format(gg = gg,
-                    size = size, 
-                    hjust = hjust, 
-                    nudge_x = nudge_x,
-                    box_padding = box_padding,
-                    point.padding = point.padding,
-                    direction = direction,
-                    alpha = alpha)
-  
-  gg <- pandemic_declared(gg = gg, df = df)
-  
-  print(gg)
-}
-
-timeline.tx <- function(df, alpha) {
-  
-  cls <- list(df = df,
-              state = "Texas",
-              title = "Confirmed cases in the state of {state} over time",
-              hjust = 1,
-              nudge_x = 20,
-              box_padding = 1,
+  cls <- list(y_lab = "# confirmed cases",
+              y = "confirmed",
+              color = "countryName",
+              df = df,
               alpha = alpha)
   
-  do.call(timeline, cls)
+  do.call(stats, cls)
 }
 
-timeline.ca <- function(df, alpha) {
+country_stats.deaths <- function(df, alpha) {
   
-  cls <- list(df = df,
-              state = "California",
-              title = "Confirmed cases in the state of {state} over time",
-              str_width = 100,
-              hjust = 1,
-              nudge_x = 20,
-              box_padding = 1,
+  cls <- list(y_lab = "# deaths",
+              y = "deaths",
+              color = "countryName",
+              df = df,
               alpha = alpha)
   
-  do.call(timeline, cls) 
+  do.call(stats, cls)
 }
 
-timeline.ny <- function(df, alpha) {
-  
-  cls <- list(df = df,
-              state = "New York",
-              title = "Confirmed cases in the state of {state} over time",
-              str_width = 100,
-              hjust = 1,
-              nudge_x = 20,
-              box_padding = 1,
-              alpha = alpha)
-  
-  do.call(timeline, cls)
+widget.country_timeline_switch <- function() {
+  shinyWidgets::switchInput(
+    inputId = "country_show_timeline"
+  )
 }
 
-timeline.us <- function(df, alpha) {
+widget.country_ndays_slider <- function() {
   
-  cls <- list(df = df,
-              country = "US",
-              title = "Confirmed cases in the United States over time",
-              hjust = 1,
-              nudge_x = 20,
-              alpha = alpha)
+  shiny::sliderInput(inputId = "country_last_x_days",
+                     label = "# most recent days",
+                     min = min(world$ndays),
+                     max = max(world$ndays),
+                     step = 1,
+                     value = max(world$ndays))
   
-  do.call(timeline, cls)
 }
 
-timeline.china <- function(df, alpha) {
-  
-  cls <- list(df = df,
-              country = "China",
-              title = "Confirmed cases in {country} over time",
-              alpha = alpha)
-  
-  do.call(timeline, cls)
+widget.country_picker <- function() {
+  shinyWidgets::pickerInput(
+    inputId = "countriesGroup", 
+    label = "Country",
+    choices = sapply(unique(world$countryName),
+                     FUN = function(x) x,
+                     USE.NAMES = TRUE, simplify = FALSE), 
+    options = list(
+      `actions-box` = TRUE, 
+      size = 10,
+      `selected-text-format` = "count > 3",
+      `live-search` = TRUE
+    ), 
+    multiple = TRUE,
+    selected = c("US", "China", "Italy", "South Korea")
+  )
 }
 
-timeline.italy <- function(df, alpha) {
-  
-  cls <- list(df = df,
-              country = "Italy",
-              title = "Confirmed cases in {country} over time",
-              str_width = 100,
-              hjust = 1,
-              nudge_x = 20,
-              box_padding = 1,
-              alpha = alpha)
-  
-  do.call(timeline, cls)
+tabBox.country <- function() {
+  shinydashboard::tabBox(side = "left",
+                         selected = "country_tab1",
+                         width = 9,
+                         shiny::tabPanel(value = "country_tab1",
+                                         title = "Confirmed cases",
+                                         shiny::plotOutput("confirmed_country_plot")),
+                         shiny::tabPanel(value = "country_tab2",
+                                         title = "Deaths",
+                                         shiny::plotOutput("deaths_country_plot"))
+                         )
 }
 
-timeline_world.ui <- function() {
+country_stats.ui <- function() {
 
   shiny::fluidRow(
-    shiny::column(
-      width = 2,
-      shiny::radioButtons(
-        "choose_country",
-        label = shiny::h3("Country"),
-        width = "100px",
-        choices = list(
-          "US" = "us",
-          "China" = "china",
-          "Italy" = "italy"
-        ),
-        selected = "us"
-      ),
-      shiny::h3("Show timeline"),
-      shinyWidgets::switchInput(
-        inputId = "show_timeline"
-        # label = "Show timeline",
-        # onStatus = "#e54e4d"
-      )
-  ),
-
-    shiny::column(width = 8,
-                  shiny::plotOutput("timeline_world_plot"))
+    shiny::column(width = 3,
+                  shiny::h3("Show timeline"),
+                  widget.country_timeline_switch(),
+                  widget.country_ndays_slider(),
+                  widget.country_picker()),
+    shiny::column(width = 9,
+                  tabBox.country()
+    )
   )
-
 }
 
-timeline_world.server <- function(input, output, session) {
+country_stats.server <- function(input, output, session) {
 
-  df2 <- shiny::reactive({
+  country_sub <- shiny::reactive({
 
     world %>% 
-      dplyr::ungroup() %>% 
-      dplyr::mutate(countryName = tolower(countryName)) %>% 
-      dplyr::filter(countryName %in% input$choose_country)
-
-  })
-
-  output$timeline_world_plot <- shiny::renderPlot({
-
-    alpha <- ifelse(input$show_timeline, 1, 0)
-
-    cls <- structure(list(df = df2(),
-                          alpha = alpha),
-                     class = input$choose_country)
-
-    do.call(timeline, cls)
-  
-  })
-  
-}
-
-timeline_state.ui <- function() {
-  shiny::fluidRow(
-    shiny::column(width = 2,
-                  shiny::radioButtons("choose_state",
-                                      label = shiny::h3("State"),
-                                      width = "110px",
-                                      choices = list("Texas" = "tx",
-                                                     "California" = "ca",
-                                                     "New York" = "ny"),
-                                      selected = "tx")),
-    shiny::column(width = 8,
-                  shiny::plotOutput("timeline_state_plot"))
+      dplyr::filter(countryName %in% input$countriesGroup) %>% 
+      dplyr::filter(ndays <= input$country_last_x_days)
     
-  )
-}
-
-timeline_state.server <- function(input, output, session) {
+  })
   
-  output$timeline_state_plot <- shiny::renderPlot({
-    timeline_cls <- timeline_cls_list[[input$choose_state]]
-    do.call(timeline, timeline_cls)
+  country_alpha <- shiny::reactive({
+    alpha <- ifelse(input$country_show_timeline, 1, 0)
+  })
+  
+  output$confirmed_country_plot <- shiny::renderPlot({
+
+    country_stats.confirmed(df = country_sub(),
+                            alpha = country_alpha())
+  })
+  
+  output$deaths_country_plot <- shiny::renderPlot({
+
+    country_stats.deaths(df = country_sub(),
+                         alpha = country_alpha())
+  })
+
+  output$tests_country_plot <- shiny::renderPlot({
+
+    country_stats.tests(df = country_sub(),
+                        alpha = country_alpha())
   })
   
 }
