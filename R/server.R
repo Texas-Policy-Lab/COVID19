35ae89ef3bb4e9181699f4e486df945df7018478
Joli Holmes <jh111@ad.rice.edu>
2020-03-25 12:51:02 -0500

    updated server with new function names

--- R/server.R
+++ R/server.R
@@ -1,6 +1,5 @@
 #' @title TPL Server
 server <- function(input, output, session) {
-  timeline_world.server(input, output, session)
-  timeline_state.server(input, output, session)
+  country_stats.server(input, output, session)
   state_stats.server(input, output, session)
 }
