7c77d0656c0bf6404aa8d6655824ff0e5f2954c6
Joli Holmes <jh111@ad.rice.edu>
2020-03-26 14:59:39 -0500

    grouped_by date, state, county and then took to last row, because there are some duplicate rows at the county level

--- R/data_management.R
+++ R/data_management.R
@@ -7,58 +7,59 @@ create_data.default <- function(...) {
   confirmed_df <- usa_facts_data.confirmed() %>% 
     tidyr::gather(Date, confirmed, -c(countyFIPS, `County Name`, State, stateFIPS)) %>%
     dplyr::mutate(Date = gsub("X", "", Date)
-                  ,Date = gsub("[.]", "/", Date)
-                  ,Date = lubridate::mdy(Date))
+                 ,Date = gsub("[.]", "/", Date)
+                 ,Date = lubridate::mdy(Date))
 
   deaths_df <- usa_facts_data.deaths() %>% 
     tidyr::gather(Date, deaths, -c(countyFIPS, `County Name`, State, stateFIPS)) %>% 
     dplyr::mutate(Date = gsub("X", "", Date)
-                  ,Date = gsub("[.]", "/", Date)
-                  ,Date = lubridate::mdy(Date))
-
+                 ,Date = gsub("[.]", "/", Date)
+                 ,Date = lubridate::mdy(Date))
+  
   df <- confirmed_df %>%
     dplyr::left_join(deaths_df)
-
+  
 }
 
 #' @title Create county level data
 #' @export
 create_data.county <- function(write = FALSE, day100 = as.Date("2020-1-18"),
                                pth = "./data/xwalk_state_stateAbb.csv", ...) {
-
+  
   county <- create_data.default() %>%
+    dplyr::group_by(countyFIPS, State, Date) %>%
+    dplyr::slice(dplyr::n()) %>% 
     dplyr::arrange(Date, State) %>%
     dplyr::left_join(census.county_pop()) %>%
     dplyr::mutate(confirm_per_100k = (confirmed/pop)*100000
                   ,death_per100k = (deaths/pop)*100000) %>% 
-    dplyr::arrange(State, countyFIPS, Date) %>% 
-    dplyr::group_by(State, countyFIPS) %>% 
+    dplyr::arrange(countyFIPS, State, Date) %>% 
+    dplyr::group_by(countyFIPS, State) %>% 
     dplyr::mutate(confirmed_lag = dplyr::lag(confirmed)
-                  ,deaths_lag = dplyr::lag(deaths)
-                  ,dy_confirmed = confirmed - confirmed_lag
-                  ,dy_deaths = deaths - deaths_lag
-                  ,pct_chg_confirmed = (dy_confirmed/confirmed)*100
-                  ,pct_chg_deaths = (dy_deaths/deaths)*100
-                  ,ndayssince100 = Date - day100
+                 ,deaths_lag = dplyr::lag(deaths)
+                 ,dy_confirmed = confirmed - confirmed_lag
+                 ,dy_deaths = deaths - deaths_lag
+                 ,pct_chg_confirmed = (dy_confirmed/confirmed)*100
+                 ,pct_chg_deaths = (dy_deaths/deaths)*100
+                 ,ndayssince100 = Date - day100
+                 ,ndays = seq(1, dplyr::n())
     ) %>% 
     dplyr::arrange(desc(Date)) %>% 
-    dplyr::mutate(ndays = seq(1, dplyr::n())
-    ) %>%
     dplyr::rename(countyName = `County Name`,
                   stateAbb = State) %>% 
     dplyr::left_join(read.csv(here::here(pth), stringsAsFactors = FALSE))
-
+  
   if(write) {
     write.csv(county, file.path("data", "county.csv"), row.names = FALSE)
   }
-
+  
   return(county)
 }
 
 #' @title Create state level data
 #' @export
 create_data.state <- function(write = FALSE, day100 = as.Date("2020-1-18"), ...) {
-
+  
   state <- create_data.default() %>%
     dplyr::arrange(Date, State) %>%
     dplyr::group_by(Date, State, stateFIPS) %>%
@@ -68,37 +69,37 @@ create_data.state <- function(write = FALSE, day100 = as.Date("2020-1-18"), ...)
     dplyr::left_join(census.state_pop()) %>%
     dplyr::left_join(testing()) %>%
     dplyr::mutate(confirm_per_100k = (confirmed/pop)*100000
-                 ,deaths_per100k = (deaths/pop)*100000
-                 ,tests_per_100k = (total_tests/pop)*100000
-                 ,positive_per100k = (positive/pop)*100000
-                 ,negative_per100k = (negative/pop)*100000
-                 ,pending_per100k = (pending/pop)*100000
+                  ,deaths_per100k = (deaths/pop)*100000
+                  ,tests_per_100k = (total_tests/pop)*100000
+                  ,positive_per100k = (positive/pop)*100000
+                  ,negative_per100k = (negative/pop)*100000
+                  ,pending_per100k = (pending/pop)*100000
     ) %>% 
     dplyr::arrange(State, Date) %>% 
     dplyr::group_by(State) %>% 
     dplyr::mutate(confirmed_lag = dplyr::lag(confirmed)
-                 ,deaths_lag = dplyr::lag(deaths)
-                 ,tests_lag = dplyr::lag(total_tests)
-                 ,positive_lag = dplyr::lag(positive)
-                 ,negative_lag = dplyr::lag(negative)
-                 ,pending_lag = dplyr::lag(pending)
-                 ,dy_confirmed = confirmed - confirmed_lag
-                 ,dy_deaths = deaths - deaths_lag
-                 ,dy_tests = total_tests - tests_lag
-                 ,dy_positive = positive - positive_lag
-                 ,dy_negative = negative - negative_lag
-                 ,pct_chg_confirmed = (dy_confirmed/confirmed)*100
-                 ,pct_chg_deaths = (dy_deaths/deaths)*100
-                 ,pct_chg_tests = (dy_tests/total_tests)*100
-                 ,pct_chg_postive = (dy_positive/positive)*100
-                 ,pct_chg_negative = (dy_negative/negative)*100
-                 ,ndayssince100 = Date - day100
+                  ,deaths_lag = dplyr::lag(deaths)
+                  ,tests_lag = dplyr::lag(total_tests)
+                  ,positive_lag = dplyr::lag(positive)
+                  ,negative_lag = dplyr::lag(negative)
+                  ,pending_lag = dplyr::lag(pending)
+                  ,dy_confirmed = confirmed - confirmed_lag
+                  ,dy_deaths = deaths - deaths_lag
+                  ,dy_tests = total_tests - tests_lag
+                  ,dy_positive = positive - positive_lag
+                  ,dy_negative = negative - negative_lag
+                  ,pct_chg_confirmed = (dy_confirmed/confirmed)*100
+                  ,pct_chg_deaths = (dy_deaths/deaths)*100
+                  ,pct_chg_tests = (dy_tests/total_tests)*100
+                  ,pct_chg_postive = (dy_positive/positive)*100
+                  ,pct_chg_negative = (dy_negative/negative)*100
+                  ,ndayssince100 = Date - day100
     ) %>%
     dplyr::arrange(desc(Date)) %>% 
     dplyr::mutate(ndays = seq(1, dplyr::n())
     ) %>%
     dplyr::rename(stateAbb = State)
-
+  
   if(write) {
     write.csv(state, file.path("data", "state.csv"), row.names = FALSE)
   }
@@ -109,24 +110,24 @@ create_data.state <- function(write = FALSE, day100 = as.Date("2020-1-18"), ...)
 #' @title Create usa-level data
 #' @export
 create_data.usa <- function(write = FALSE, day100 = as.Date("2020-1-18"), ...) {
-
+  
   usa <- create_data.default() %>%
     dplyr::arrange(Date, State) %>%
     dplyr::group_by(Date) %>%
     dplyr::summarise(deaths = sum(deaths)
-                    ,confirmed = sum(confirmed)) %>% 
+                     ,confirmed = sum(confirmed)) %>% 
     dplyr::arrange(Date) %>% 
     dplyr::mutate(confirmed_lag = dplyr::lag(confirmed)
-                 ,deaths_lag = dplyr::lag(deaths)
-                 ,dy_confirmed = confirmed - confirmed_lag
-                 ,dy_deaths = deaths - deaths_lag
-                 ,pct_chg_confirmed = (dy_confirmed/confirmed)*100
-                 ,pct_chg_deaths = (dy_deaths/deaths)*100
-                 ,ndayssince100 = Date - day100) %>% 
+                  ,deaths_lag = dplyr::lag(deaths)
+                  ,dy_confirmed = confirmed - confirmed_lag
+                  ,dy_deaths = deaths - deaths_lag
+                  ,pct_chg_confirmed = (dy_confirmed/confirmed)*100
+                  ,pct_chg_deaths = (dy_deaths/deaths)*100
+                  ,ndayssince100 = Date - day100) %>% 
     dplyr::arrange(desc(Date)) %>% 
     dplyr::mutate(ndays = seq(1, dplyr::n())
-                  )
-
+    )
+  
   if(write) {
     write.csv(usa, file.path("data", "usa.csv"), row.names = FALSE)
   }
