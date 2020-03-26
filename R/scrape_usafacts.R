22a2cc4fd0b63d98f5f06023a74764a18fcf821d
Joli Holmes <jh111@ad.rice.edu>
2020-03-26 15:00:09 -0500

    deleted whitespace

--- R/scrape_usafacts.R
+++ R/scrape_usafacts.R
@@ -21,7 +21,7 @@ usa_facts_data.default <- function(url = "https://static.usafacts.org/public/dat
 #' @param csv_name string. Default is "covid_confirmed_usafacts.csv"
 #' @export
 usa_facts_data.confirmed <- function(csv_name = "covid_confirmed_usafacts.csv") {
-
+  
   usa_facts_data(csv_name = csv_name)
 }
 
@@ -30,6 +30,6 @@ usa_facts_data.confirmed <- function(csv_name = "covid_confirmed_usafacts.csv") 
 #' @param csv_name string. Default is "covid_deaths_usafacts.csv"
 #' @export
 usa_facts_data.deaths <- function(csv_name = "covid_deaths_usafacts.csv") {
-
+  
   usa_facts_data(csv_name = csv_name)
 }
