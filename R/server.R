2ee97271d27bcef02a1141be40957e0abae38ec1
Joli Holmes <jh111@ad.rice.edu>
2020-03-26 15:00:27 -0500

    added county data

--- R/server.R
+++ R/server.R
@@ -2,4 +2,5 @@
 server <- function(input, output, session) {
   country_stats.server(input, output, session)
   state_stats.server(input, output, session)
+  county_stats.server(input, output, session)
 }
